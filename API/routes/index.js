6cf022ff68eb108e71a31ee68498287bf7213ddb
Artizmo <Brian@DESKTOP-PH30CK2>
2020-10-07 22:09:08 -0500

    sets up eslinting and lints the api config files

--- API/routes/index.js
+++ API/routes/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import moviesRoutes from './moviesRoutes';
+
 const app = express();
 
 app.use('/movies', moviesRoutes);
--- API/server.js
+++ API/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import routes from './routes';
+
 const app = express();
 dotenv.config();
 
@@ -11,15 +12,15 @@ app.use(['/api', '/api*'], (req, res) => {
     .status(404)
     .json({
       message: 'No resources available',
-      status: 404
-    })
+      status: 404,
+    });
 });
 
 app.get('/', (req, res) => {
   res.json({
     message: 'Looks good!',
-    status: 200
-  })
+    status: 200,
+  });
 });
 
 app.listen(process.env.PORT);
